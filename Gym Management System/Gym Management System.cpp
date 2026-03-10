60b2e52f3ca129f8fbbbfb90a0eb51a8861bbb84
Youssef Shafik <youssefmaisara33@gmail.com>
2026-03-10 14:55:28 +0200

    i added classes

--- Gym Management System/Gym Management System.cpp
+++ Gym Management System/Gym Management System.cpp
@@ -2,6 +2,69 @@
 #include <iostream>
 using namespace std;
 
+class Member_Management
+{
+public:
+
+
+private:
+
+};
+
+class Trainer_Management
+{
+public:
+
+
+private:
+
+};
+
+class Membership_Plan_Management
+{
+public:
+
+
+private:
+
+};
+
+class Workout_Program_Management
+{
+public:
+
+
+private:
+
+};
+
+class Attendance_Tracking
+{
+public:
+
+
+private:
+
+};
+
+class Billing_System
+{
+public:
+
+
+private:
+
+};
+
+class Reports
+{
+public:
+
+
+private:
+
+};
+
 int main()
 {
 	cout << "Hello World!\n";
